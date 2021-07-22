1f2f0edf8ca8d49fbe90fbfa35bd4f94df8aa475
Shaswata-5129 <shaswatamondalfcs1@gmail.com>
2021-07-22 20:05:23 +0530

    Color given and auto corrected. Error in equal

--- Gui.py
+++ Gui.py
@@ -77,8 +77,7 @@ class CalcRun(Widget):
             print(num_equal)
             for i in num_equal:
                 answer = answer + float(i)
-            # self.ids.num_input.text = str(answer)
-            self.answer(answer)
+            answer = str(answer)
 
         elif '-' in prior:
             if prior[-1] == '-':
@@ -87,9 +86,8 @@ class CalcRun(Widget):
                 num_equal = prior.split('-')
                 print(num_equal)
                 # for i in num_equal:
-                answer = int(num_equal[0]) - int(num_equal[1])
-            # self.ids.num_input.text = str(answer)
-            self.answer(answer)
+                answer = float(num_equal[0]) - float(num_equal[1])
+                answer = str(answer)
 
         elif '×' in prior:
             if prior[-1] == '×':
@@ -98,9 +96,8 @@ class CalcRun(Widget):
                 num_equal = prior.split('×')
                 print(num_equal)
                 # for i in num_equal:
-                answer = int(num_equal[0]) * int(num_equal[1])
-            # self.ids.num_input.text = str(answer)
-            self.answer(answer)
+                answer = float(num_equal[0]) * float(num_equal[1])
+                answer = str(answer)
 
         elif '÷' in prior:
             if prior[-1] == '÷':
@@ -109,19 +106,28 @@ class CalcRun(Widget):
                 num_equal = prior.split('÷')
                 print(num_equal)
                 # for i in num_equal:
-                answer = int(num_equal[0]) / int(num_equal[1])
-            # self.ids.num_input.text = str(answer)
-            self.answer(answer)
+                answer = float(num_equal[0]) / float(num_equal[1])
+                answer = str(answer)
+
+
+        if ('.') in answer:
+            find = answer.index('.')
+            if answer[find + 1] == '0':
+                answer = answer[:find]
+            else:
+                pass
+        self.ids.num_input.text = answer
+
     # As all the answers is in float for point answers, this will determine if the point has any value or not, if not then Simply remove point
-    def answer(self,answer):
-        prior = self.ids.num_input.text      # To put the sign after the auto funcs
+    def answer(self, answer):
+        prior = self.ids.num_input.text  # To put the sign after the auto funcs
         print(prior, 'FInal')
         if prior[-1] == '+' or '-' or '÷' or '×':
             sign = prior[-1]
-            print(prior[-1],'prior-1')# To put the sign after the auto funcs
+            print(prior[-1], 'prior-1')  # To put the sign after the auto funcs
         else:
             sign = 0
-            print(sign,'sign')
+            print(sign, 'sign')
         answer = str(answer)
         if ('.') in answer:
             find = answer.index('.')
@@ -129,68 +135,45 @@ class CalcRun(Widget):
                 answer = answer[:find]
             else:
                 pass
-        self.ids.num_input.text = answer+sign
+        self.ids.num_input.text = answer + sign
+
     # To print answer if we continue pressing buttons
-    def auto(self,num_list):
+    def auto(self, num_list):
         # Differentiating LABEL with INNER NUMBER
         prior = self.ids.num_input.text
-        if prior[-1] =='+' or '-':
+        if prior[-1] == '+' or '-':
             answer = 0
         else:
             answer = 1
         print(prior)
         # Checking the condition for auto sum
         num_list = num_list[:-1]
-        print(num_list,'num_list')
-        if len(num_list)==2:
+        print(num_list, 'num_list')
+        if len(num_list) == 2:
 
             # For Addition of auto sum
-            if prior[-1]=='+':
+            if prior[-1] == '+':
                 for i in num_list:
                     answer = answer + float(i)
             # For Subtraction of Auto Sum
-            elif prior[-1]=='-':
-                answer = float(num_list[0])-float(num_list[1])
+            elif prior[-1] == '-':
+                answer = float(num_list[0]) - float(num_list[1])
             # For Multiplication of Auto Sum
-            elif prior[-1]=='×':
-                answer = float(num_list[0])*float(num_list[1])
+            elif prior[-1] == '×':
+                answer = float(num_list[0]) * float(num_list[1])
             # For Division of Auto Sum
-            elif prior[-1]=='÷':
-                answer = float(num_list[0])/float(num_list[1])
-
-
+            elif prior[-1] == '÷':
+                answer = float(num_list[0]) / float(num_list[1])
 
             self.answer(answer)
-        '''
-        if prior[-1]=='+':
-            prior = prior[:-1]
-            print(prior)
-            if '+' in prior:
-                num_add = prior.split('+')
-                print(num_add,'num_add')
-                if len(num_add) == 2:
-                    for i in num_add:
-                        answer = answer + float(i)
-                    answer = str(answer) + '+'
-                    self.answer(answer)
-        
-        else:
-            pass
-        '''
-        '''
-        elif '-' in prior:
-            num_sub = prior.split('-')
-        elif '×' in prior:
-            num_mul = prior.split('×')
-        elif '÷' in prior:
-            num_div = prior.split('÷')
-     #   print(num_add,'num_add')
-        '''
 
-# to clear the total screen
+
+
+    # to clear the total screen
     def clear(self):
         self.ids.num_input.text = '0'
-# to assign the numbers and signs and all button according to the label
+
+    # to assign the numbers and signs and all button according to the label
     def button_press(self, button):
         prior = self.ids.num_input.text
         if prior == '0':
@@ -204,7 +187,7 @@ class CalcRun(Widget):
         if prior == '0':
             pass
         else:
-            if len(prior) ==1:
+            if len(prior) == 1:
                 self.ids.num_input.text = '0'
             else:
                 self.ids.num_input.text = f'{prior[:-1]}'
@@ -215,13 +198,13 @@ class CalcRun(Widget):
     def back(self):
         pass
 
-# To correct the sign function of calculator
-    def math_sign(self,sign):
+    # To correct the sign function of calculator
+    def math_sign(self, sign):
         num_list = []
         prior = self.ids.num_input.text
         if prior == '0':
             self.ids.num_input.text = ''
-# If last str is +,-,*,/ skip
+        # If last str is +,-,*,/ skip
         elif prior[-1] == '÷':
             pass
         elif prior[-1] == '+':
@@ -230,20 +213,20 @@ class CalcRun(Widget):
             pass
         elif prior[-1] == '×':
             pass
-# if signs are not there it will simply add the strs
+        # if signs are not there it will simply add the strs
         else:
             prior = f'{prior}{sign}'
             self.ids.num_input.text = prior
             num_list = prior.split(sign)
-            print('numList',num_list)
+            print('numList', num_list)
             self.auto(num_list)
-
 
 
 # Mainframe of pogram
 class CalcApp(App):
     def build(self):
         return CalcRun()
+
 
 # running the programe
 if __name__ == '__main__':
