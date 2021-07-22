af21e4afdd70cb2d73130a9ccc3ab2b1a7a714cf
Shaswata-5129 <shaswatamondalfcs1@gmail.com>
2021-07-22 14:20:21 +0530

    code shorted and auto corrected

--- Gui.py
+++ Gui.py
@@ -67,7 +67,7 @@ class CalcGrid(GridLayout):
 
 
 class CalcRun(Widget):
-
+    # To find the answer if we press =
     def equal(self):
         answer = 0.00
 
@@ -115,8 +115,10 @@ class CalcRun(Widget):
                 answer = int(num_equal[0]) / int(num_equal[1])
             # self.ids.num_input.text = str(answer)
             self.answer(answer)
-
-    def answer(self, answer):
+    # As all the answers is in float for point answers, this will determine if the point has any value or not, if not then Simply remove point
+    def answer(self,answer):
+        prior = self.ids.num_input.text      # To put the sign after the auto funcs
+        sign = prior[-1]                      # To put the sign after the auto funcs
         answer = str(answer)
         if ('.') in answer:
             find = answer.index('.')
@@ -124,53 +126,68 @@ class CalcRun(Widget):
                 answer = answer[:find]
             else:
                 pass
-        self.ids.num_input.text = answer
-
-    def auto(self):
+        self.ids.num_input.text = answer+sign
+    # To print answer if we continue pressing buttons
+    def auto(self,num_list):
+        # Differentiating LABEL with INNER NUMBER
         prior = self.ids.num_input.text
+        if prior[-1] =='+' or '-':
+            answer = 0
+        else:
+            answer = 1
         print(prior)
-        print(len(prior))
-        answer = 0
-        if len(prior) >= 3:
-            if '+' in prior:
+        # Checking the condition for auto sum
+        num_list = num_list[:-1]
+        print(num_list,'num_list')
+        if len(num_list)==2:
 
-                num_add = prior.split('+')
-                del num_add[-1]
-                for i in num_add:
+            # For Addition of auto sum
+            if prior[-1]=='+':
+                for i in num_list:
                     answer = answer + float(i)
-                self.ids.num_input.text = str(answer) + '+'
-
-        #if len(prior) > 3:
-            elif '-' in prior:
-                num_sub = prior.split('-')
-                del num_sub[-1]
-                print('num sub', num_sub)
-                # for i in num_add:
-                answer = int(num_sub[0]) - int(num_sub[1])
-                self.ids.num_input.text = str(answer) + '-'
-
-            elif '×' in prior:
-                num_mul = prior.split('×')
-                del num_mul[-1]
-                print('num mul', num_mul)
-                answer = 1
-                for i in num_mul:
-                    answer = answer * i
-                self.ids.num_input.text = str(answer) + '×'
-
-            elif '÷' in prior:
-                num_div = prior.split('÷')
-                del num_div[-1]
-                print('num div', num_div)
-                # for i in num_add:
-                answer = int(num_div[0]) / int(num_div[1])
-                self.ids.num_input.text = str(answer) + '÷'
+            # For Subtraction of Auto Sum
+            elif prior[-1]=='-':
+                answer = float(num_list[0])-float(num_list[1])
+            # For Multiplication of Auto Sum
+            elif prior[-1]=='×':
+                answer = float(num_list[0])*float(num_list[1])
+            # For Division of Auto Sum
+            elif prior[-1]=='÷':
+                answer = float(num_list[0])/float(num_list[1])
 
 
 
+            self.answer(answer)
+        '''
+        if prior[-1]=='+':
+            prior = prior[:-1]
+            print(prior)
+            if '+' in prior:
+                num_add = prior.split('+')
+                print(num_add,'num_add')
+                if len(num_add) == 2:
+                    for i in num_add:
+                        answer = answer + float(i)
+                    answer = str(answer) + '+'
+                    self.answer(answer)
+        
+        else:
+            pass
+        '''
+        '''
+        elif '-' in prior:
+            num_sub = prior.split('-')
+        elif '×' in prior:
+            num_mul = prior.split('×')
+        elif '÷' in prior:
+            num_div = prior.split('÷')
+     #   print(num_add,'num_add')
+        '''
+
+# to clear the total screen
     def clear(self):
         self.ids.num_input.text = '0'
-
+# to assign the numbers and signs and all button according to the label
     def button_press(self, button):
         prior = self.ids.num_input.text
         if prior == '0':
@@ -179,13 +196,15 @@ class CalcRun(Widget):
         else:
             self.ids.num_input.text = f'{prior}{button}'
 
-    def backspace(self, button):
+    def backspace(self):
         prior = self.ids.num_input.text
         if prior == '0':
-            self.ids.num_input.text = ''
-            self.ids.num_input.text = f'{button}'
+            pass
         else:
-            self.ids.num_input.text = f'{prior}{button}'
+            if len(prior) ==1:
+                self.ids.num_input.text = '0'
+            else:
+                self.ids.num_input.text = f'{prior[:-1]}'
 
     def extra(self):
         pass
@@ -193,11 +212,13 @@ class CalcRun(Widget):
     def back(self):
         pass
 
+# To correct the sign function of calculator
     def math_sign(self,sign):
+        num_list = []
         prior = self.ids.num_input.text
         if prior == '0':
             self.ids.num_input.text = ''
-
+# If last str is +,-,*,/ skip
         elif prior[-1] == '÷':
             pass
         elif prior[-1] == '+':
@@ -206,18 +227,21 @@ class CalcRun(Widget):
             pass
         elif prior[-1] == '×':
             pass
-
+# if signs are not there it will simply add the strs
         else:
-            self.ids.num_input.text = f'{prior}{sign}'
-            self.auto()
+            prior = f'{prior}{sign}'
+            self.ids.num_input.text = prior
+            num_list = prior.split(sign)
+            print('numList',num_list)
+            self.auto(num_list)
 
 
 
-
+# Mainframe of pogram
 class CalcApp(App):
     def build(self):
         return CalcRun()
 
-
+# running the programe
 if __name__ == '__main__':
     CalcApp().run()
